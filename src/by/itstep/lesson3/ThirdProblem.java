8ce3beb40c61aa37318448b7ce613a56e5130d6b
UncleAlexx <itsgdstorm@gmail.com>
2026-09-18 01:24:25 +0300

    rename  by.itstep.lesson3.ThirdProblem.getSumOfOddEven to by.itstep.lesson3.ThirdProblem.getSumDifferenceOfElemsWithEvenOddIndexes & adding back lesson1 occasionally deleted

---
+++ src/by/itstep/lesson3/ThirdProblem.java
@@ -0,0 +1,17 @@
+package by.itstep.lesson3;
+
+public class ThirdProblem {
+    public int getSumDifferenceOfElemsWithEvenOddIndexes (int [] numbers){
+        if(numbers == null || numbers.length == 0)
+            return 0;
+        int sum = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+
+            if((i & 1) == 1)
+                sum -= numbers[i];
+            else
+                sum += numbers[i];
+        }
+        return  Math.abs(sum);
+    }
+}
